4d98986fec910c095017f6aef51784ee1f5efe5c
krtps57 <43800163+jkerick87@users.noreply.github.com>
2024-09-26 20:21:28 -0300

    aula4
    aula sobre manipulação de listas

---
+++ aulas/desafio2.py
@@ -0,0 +1,16 @@
+minhaLista = [76,92.3,"oi",True,4,76]
+print(minhaLista)
+
+minhaLista.append("pera")
+minhaLista.append(76)
+print(minhaLista)
+minhaLista.insert(0,99)
+print(minhaLista)
+indice = minhaLista.index("oi")
+print("O indice:",indice)
+minhaLista.remove(True)
+print(minhaLista)
+
+uma_lista = [4,2,8,6,5]
+uma_lista = uma_lista  + ['gato','bode','bola']
+print(uma_lista)
--- aulas/lista.py
+++ aulas/lista.py
@@ -1,4 +1,34 @@
 lista1 = []
-lista1 = [1,"2",3]
-print(type(lista1),lista1)
+# lista1 = [1,"2",3]
+# print(type(lista1),lista1)
+i=-1
+lista2 = [123,"321","SENAC"]
 
+lista3 = ["C",4.65,True,"True","Vamos Aprender",["outra","lista","interna"],lista2]
+print(lista3[2:6:2])
+print(lista3[3:])
+print(lista3[::i])
+print(len(lista3))
+print(lista3 [: :])
+
+print("LISTA 1:", lista1)
+lista1.append("Python")
+lista1.append("Java")
+lista1.append("PHP")
+lista1.append("C#")
+print(lista1)
+
+lista1.insert(2,"C++")
+print(lista1)
+
+lista1.remove("Java")
+print(lista1)
+
+#indice = lista1("PHP")
+#print(indice)
+#print(L)
+
+a = [1,2,3]
+b = a[:]
+b[0] = 5
+print(a[3])
