3eafe42c9db316d80fe7cbc79b460b2b16b5eafa
jkerick87 <jkerick@gmail.com>
2024-10-08 11:27:29 -0300

    projeto banco
    projeto banco finalizado

--- projeto/operacoes.py
+++ projeto/operacoes.py
@@ -7,6 +7,19 @@ def menu():
     print("3. Depositar")
     print("4. Sacar")
     print("5. Sair")
+    
+def carregar_contas():
+    try:
+        with open('contas.txt', 'r') as file:
+            return json.load(file)
+    except FileNotFoundError: 
+        return {}
+    except json.JSONDecodeError: 
+        return {}
+    
+def salvar_arquivo(contas):
+    with open('contas.txt', 'w') as file:
+        json.dump(contas, file)
 
 def criar_conta(contas):
     nome = input("Digite o nome do titular: ")
@@ -18,43 +31,45 @@ def criar_conta(contas):
         "saldo": saldo
     }
 
-    # Salvar no arquivo
-    with open('contas.txt', 'w') as file:
-        json.dump(contas, file)
-
+    salvar_arquivo(contas)
     print("Conta criada com sucesso!")
 
 def consultar_conta(contas):
     numero_conta = input("Digite o número da conta para consulta: ")
     if numero_conta in contas:
         conta = contas[numero_conta]
-        print(f"Nome: {conta['nome']}, Saldo: {conta['saldo']:.2f}")
-    # else:
-    #     print("Conta não encontrada.")
+        print(f"Nome: {conta['nome'].upper()}, Saldo: {conta['saldo']:.2f}")
+    else:
+        print("Conta não encontrada.")
+
+def depositar_dinheiro(contas):
+    conta_alvo = input("Digite a conta: ")
+    
+    if conta_alvo in contas :
+        novo_valor = float(input("Digite o valor do deposito:"))
+        if novo_valor > 0:
+            contas[conta_alvo]['saldo'] += novo_valor
+            print(f"Depósito de {novo_valor} realizado com sucesso")
+            salvar_arquivo(contas)
+        else:
+            print("Valor inválido. O depósito deve ser maior que zero.")
+    else:
+        print("Conta não encontrada.")
         
-    return conta,conta['nome'],conta['saldo']
 
-def carregar_contas():
-    try:
-        with open('contas.txt', 'r') as file:
-            return json.load(file)
-    except FileNotFoundError:
-        return {}
-    
-def depositar_dinheiro(contas,conta_alvo,novo_valor):
-    
-    for conta in contas:
-        print(f"CONTA: {contas}, Novo Valor: {novo_valor}")
-        if conta_alvo in conta :
-            conta = contas[conta_alvo]
-            conta['saldo'] += novo_valor
-            print(f"CONTA: {conta['saldo']}")
-    #         
-    #         print(f"Valor atualizado para a conta {conta_alvo}: {novo_valor}")
-    #         return
-    # print(f"Conta {conta_alvo} não encontrada.")
+def sacar_dinheiro(contas):
+   conta_alvo = input("Digite a conta: ")
+   if conta_alvo in contas :
+        novo_valor = float(input("Digite o valor do saque:"))
+        if contas[conta_alvo]['saldo'] > novo_valor:
+            contas[conta_alvo]['saldo'] -= novo_valor
+            print(f"Saque de {novo_valor} realizado com sucesso")
+            salvar_arquivo(contas)
+        else:
+            print("Saldo Insuficiente!!!")
+   else:
+    print("Conta não encontrada.")
        
-
 def main():
     contas = carregar_contas()
 
@@ -63,25 +78,19 @@ def main():
         opcao = input("Escolha uma opção: ")
         
         if opcao == '1':
-            try: 
-                criar_conta(contas)
-            except UnboundLocalError:
-                print("Conta não existe.") 
-                 
+            criar_conta(contas)
+
         elif opcao == '2':
             try:
-                conta,nome,saldo = consultar_conta(contas)
-                #print(f'Saldo: {saldo}')
+                consultar_conta(contas)
             except UnboundLocalError:
-                print("Conta não existe.")
+                print("Conta não existe!!!")
                 
         elif opcao == '3':
-            conta_alvo = input("Digite a conta: ")
-            novo_valor = int(input("Digite o valor do deposito: "))
-            depositar_dinheiro(contas,conta_alvo,novo_valor)
-            #print(f"Conta:{conta}, nome:{nome}, Saldo: {saldo} ")
-            
-            # depositar_dinheiro()
+                depositar_dinheiro(contas)
+        
+        elif opcao == '4':
+            sacar_dinheiro(contas) 
             
         elif opcao == '5':
             print("Saindo...")
